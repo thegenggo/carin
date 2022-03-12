cf117b4864b7197c951a311caffdbc29dc335876
Kritsanaphong <kritsanaphong-keang@hotmail.com>
2022-03-13 00:25:20 +0700

    add responsive

--- src/main/ts/components/SetupPage.tsx
+++ src/main/ts/components/SetupPage.tsx
@@ -1,4 +1,4 @@
-import React  from "react";
+import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import './SetupPage.css';
 import pfizer from './images/pfizer.png';
@@ -20,41 +20,43 @@ function SetupPage() {
 
     return (
         <div className="setupPage">
-            <h1>Set Genetic Code</h1>
-            <div>
-                <div className="row">
-                    <Link to={"pfizer"}>
-                        <div className="setupPage organismPortrait">
-                            <img src={pfizer} className="pfizer"></img>
-                        </div>
-                    </Link>
-                    <Link to={"moderna"}>
-                        <div className="setupPage organismPortrait">
-                            <img src={moderna} className="pfizer"></img>
-                        </div>
-                    </Link>
-                    <Link to={"sinovac"}>
-                        <div className="setupPage organismPortrait">
-                            <img src={sinovac} className="pfizer"></img>
-                        </div>
-                    </Link>
-                </div>
-                <div className="row">
-                    <Link to={"alpha"}>
-                        <div className="setupPage organismPortrait">
-                            <img src={alpha} className="pfizer"></img>
-                        </div>
-                    </Link>
-                    <Link to={"beta"}>
-                        <div className="setupPage organismPortrait">
-                            <img src={beta} className="pfizer"></img>
-                        </div>
-                    </Link>
-                    <Link to={"gamma"}>
-                        <div className="setupPage organismPortrait">
-                            <img src={gamma} className="pfizer"></img>
-                        </div>
-                    </Link>
+            <div className="setupMidField">
+                <h1>Set Genetic Code</h1>
+                <div>
+                    <div className="row">
+                        <Link to={"pfizer"}>
+                            <div className="organismPortrait">
+                                <img src={pfizer} className="pfizer"></img>
+                            </div>
+                        </Link>
+                        <Link to={"moderna"}>
+                            <div className="organismPortrait">
+                                <img src={moderna} className="pfizer"></img>
+                            </div>
+                        </Link>
+                        <Link to={"sinovac"}>
+                            <div className="organismPortrait">
+                                <img src={sinovac} className="pfizer"></img>
+                            </div>
+                        </Link>
+                    </div>
+                    <div className="row">
+                        <Link to={"alpha"}>
+                            <div className="organismPortrait">
+                                <img src={alpha} className="pfizer"></img>
+                            </div>
+                        </Link>
+                        <Link to={"beta"}>
+                            <div className="organismPortrait">
+                                <img src={beta} className="pfizer"></img>
+                            </div>
+                        </Link>
+                        <Link to={"gamma"}>
+                            <div className="organismPortrait">
+                                <img src={gamma} className="pfizer"></img>
+                            </div>
+                        </Link>
+                    </div>
                 </div>
             </div>
             <img src={playButton} className="playButton" onClick={start}></img>

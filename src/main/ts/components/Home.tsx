5ec58c147f8c837622fc9a08fde2e7373262d3fb
Kritsanaphong <kritsanaphong-keang@hotmail.com>
2022-03-12 22:53:58 +0700

    add responsive

--- src/main/ts/components/Home.tsx
+++ src/main/ts/components/Home.tsx
@@ -3,18 +3,17 @@ import gameName from './images/gameName.png'
 import './Home.css'
 import { Link } from "react-router-dom";
 
-function Home (){
-    return(
-        <Link to={"setup"} className = "underline">
+function Home() {
+    return (
+        <Link to={"setup"} className="underline">
             <div className="home">
-            <div className="center">
-                <img src={gameName} className ="center-p" />
-            </div>
-            <h1 className="center">Click to start...</h1>
-            
+                <div className="center">
+                    <img src={gameName} className="center-p" />
+                    <h1>Click to start...</h1>
+                </div>
             </div>
         </Link>
-        
+
     );
 }
 export default Home;
